ca7cf054e983159b2d72e365387ecf257611a22f
Javier <aguilarjaviii14@gmail.com>
2025-04-19 16:44:43 -0700

    setting up the database

---
+++ frontend/main.py
@@ -0,0 +1,16 @@
+# Main.py intializes project.
+# Launchs UI
+# TODO Include proper documentation in the form of docstrings for classes, functions, and methods.
+
+import tkinter as tk
+from tkinter import ttk
+def main():
+    print("Start of program")
+    root = tk.Tk()
+    root.title("BudgetBuddy")
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
